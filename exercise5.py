e94dcf46ba265cd8be582c6f601ac3c2b1f456fc
nasir <refreshsho@gmail.com>
2025-01-30 11:18:40 +0330

    message

---
+++ exercise5.py
@@ -0,0 +1,13 @@
+n = int(input())
+for i in range(1, n + 1):
+    print(" " * (n), end="")
+    print("*" * (2 * i - 1), end="")
+    print("  " * (n - i), end="")  
+    print(" " * (n - i), end="")
+    print("*" * (2 * i - 1))
+for i in range(n - 1, 0, -1):
+    print(" " * (n - i), end="")
+    print("*" * (2 * i - 1), end="")
+    print("  " * (n - i), end="")  
+    print(" " * (n - i), end="")
+    print("*" * (2 * i - 1))
